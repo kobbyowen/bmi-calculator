b0a5f3e5d9d57189f374a2ccafdec1ff2d342869
Arrow98 <gm180802@gmail.com>
2025-01-13 20:23:41 -0800

    the event object used rather than the reuse of the variable name

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const maleButton = document.getElementById("male")
 const femaleButton = document.getElementById("female")
 
 maleButton.addEventListener('click',(e)=>{
-    maleButton.classList.add("active-gender")
+    e.target.classList.add("active-gender")
     femaleButton.classList.remove("active-gender")
 })
 
 femaleButton.addEventListener('click',(e)=>{
-    femaleButton.classList.add("active-gender")
+    e.target.classList.add("active-gender")
     maleButton.classList.remove("active-gender")
     
 })
