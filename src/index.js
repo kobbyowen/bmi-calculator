c49a76b699e5671c81481e3f4a96e80f5f21564c
Arrow98 <gm180802@gmail.com>
2025-01-15 12:15:20 -0800

    js code refactored, gender value saved in a variable and value object displays all necessary values

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const maleButton = document.getElementById("male");
 const femaleButton = document.getElementById("female");
-const calculateButton = document.getElementsByTagName("button")[2];
+const calculateButton = document.querySelector(
+  "form.bmi-form button[type='submit']"
+);
 const height = document.getElementById("height");
 const weight = document.getElementById("weight");
 const age = document.getElementById("age");
 const heightUnit = document.getElementById("height-unit");
 const weightUnit = document.getElementById("weight-unit");
+
+function getGenderValue() {
+  if (femaleButton.classList.contains("active-gender")) {
+    return "female";
+  }
+  if (maleButton.classList.contains("active-gender")) {
+    return "male";
+  }
+  return "";
+}
 
 maleButton.addEventListener("click", (e) => {
   e.target.classList.add("active-gender");
@@ -19,26 +31,33 @@ femaleButton.addEventListener("click", (e) => {
 
 calculateButton.addEventListener("click", (e) => {
   e.preventDefault();
+  const gender = getGenderValue();
   const heightValue = height.value;
   const weightValue = weight.value;
   const ageValue = age.value;
   const heightUnitValue = heightUnit.value;
   const weightUnitValue = weightUnit.value;
 
-  if (ageValue === "") {
-    age.classList.add("bmi-input-error");
-  }
-  if (heightValue === "") {
-    height.classList.add("bmi-input-error");
-  }
-  if (weightValue === "") {
-    weight.classList.add("bmi-input-error");
-  }
+  const requiredElements = [age, height, weight];
+
+  requiredElements.forEach((element) =>
+    element.value === ""
+      ? element.classList.add("bmi-input-error")
+      : element.classList.remove("bmi-input-error")
+  );
+  const hasEmptyFields = requiredElements.some(
+    (element) => element.value === ""
+  );
+
+  if (hasEmptyFields) return;
 
   const formData = {
-    age: ageValue,
-    height: `${heightValue}${heightUnitValue}`,
-    weight: `${weightValue}${weightUnitValue}`,
+    gender,
+    age: parseInt(ageValue),
+    height: parseInt(heightValue),
+    heightUnit: heightUnitValue,
+    weight: parseInt(weightValue),
+    weightUnit: weightUnitValue,
   };
   console.log(formData);
 });
