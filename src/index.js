07581315967c737bac2e57e5e6ac3f50dc5cd1fa
Arrow98 <gm180802@gmail.com>
2025-01-15 23:12:46 -0800

    created BMI chart using canvas and calculated all necessary values need for the application

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const weight = document.getElementById("weight");
 const age = document.getElementById("age");
 const heightUnit = document.getElementById("height-unit");
 const weightUnit = document.getElementById("weight-unit");
+let bmiValues = {};
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const values = [30, 55, 35, 40];
+const colors = ["#45a09f", "#f0a710", "#ea6170", "#8292f1"];
+
+const labels = ["Voluntary", "Robot", "Mandatory"];
 
 function getGenderValue() {
   if (femaleButton.classList.contains("active-gender")) {
@@ -60,4 +67,86 @@ calculateButton.addEventListener("click", (e) => {
     weightUnit: weightUnitValue,
   };
   console.log(formData);
+
+  const accessBmiValues = getBmiValues(formData);
+
+  dmbChart(400, 230, 165, 50, values, colors, labels, 0, accessBmiValues);
 });
+
+function getBmiValues(data) {
+  let height = 0;
+  if (data.heightUnit === "cm") {
+    height = data.height / 100;
+  } else {
+    height = data.height;
+  }
+  let weight = 0;
+  if (data.weightUnit === "Ib") {
+    weight = data.weight / 2.20462;
+  } else {
+    weight = data.weight;
+  }
+
+  const bmi = (weight / height ** 2).toFixed(2);
+  const bmiPrime = (bmi / 25).toFixed(2);
+  const pondoralIndex = (weight / height ** 3).toFixed(2);
+
+  return {
+    bmi,
+    bmiPrime,
+    pondoralIndex,
+  };
+}
+
+dmbChart(400, 230, 165, 50, values, colors, labels, 0, { bmi: "N/A" });
+
+function dmbChart(
+  cx,
+  cy,
+  radius,
+  arcwidth,
+  values,
+  colors,
+  labels,
+  selectedValue,
+  accessBmiValues
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  let tot = 0;
+  let accum = 0;
+  const PI = Math.PI;
+  const PI2 = PI * 2;
+  const offset = -PI / 2.5;
+  ctx.lineWidth = arcwidth;
+  for (let i = 0; i < values.length; i++) {
+    tot += values[i];
+  }
+  for (let i = 0; i < values.length; i++) {
+    ctx.beginPath();
+    ctx.arc(
+      cx,
+      cy,
+      radius,
+      offset + PI2 * (accum / tot),
+      offset + PI2 * ((accum + values[i]) / tot)
+    );
+    ctx.strokeStyle = colors[i];
+    ctx.stroke();
+    accum += values[i];
+  }
+  const innerRadius = 0;
+  ctx.beginPath();
+  ctx.arc(cx, cy, innerRadius, 0, PI2);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.textAlign = "center";
+  ctx.textBaseline = "bottom";
+  ctx.fillStyle = "black";
+
+  ctx.font = 35 + "px verdana";
+  if (accessBmiValues.bmi === "N/A") {
+    ctx.fillText(`BMI= ${"N/A"}`, cx, cy - innerRadius * 0.25);
+  } else {
+    ctx.fillText(`BMI= ${accessBmiValues.bmi}`, cx, cy - innerRadius * 0.25);
+  }
+}
