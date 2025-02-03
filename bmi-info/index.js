5c3abf69495e6934b4e83c54529eb99e3cd305af
Arrow98 <gm180802@gmail.com>
2025-02-02 17:25:39 -0800

    gave classes to respective divs and made code loosely coupled for future use

--- bmi-info/index.js
+++ bmi-info/index.js
@@ -1,13 +1,13 @@
-const parent = document.getElementById("bmi-main-content");
-const mainContainer = document.getElementById("bmi-content-container");
+const mainContainer = document.getElementById("bmi-content-container-wrapper");
 const childrenElements = mainContainer.children;
 const dots = document.createElement("div");
+dots.classList.add("bmi-dots-carousel");
 
 const arrayElements = Array.from(childrenElements);
 arrayElements.forEach((element) => {
   const dot = document.createElement("div");
-  dot.textContent = "T";
+  dot.classList.add("bmi-carousel-dot");
   dots.appendChild(dot);
 });
 
-parent.appendChild(dots);
+mainContainer.parent.appendChild(dots);
